3ebcc68651f6ed3cd13f51a2a9fea80a05423587
kudegras <86367802+kudegras@users.noreply.github.com>
2022-04-07 14:34:33 +0800

    Add files via upload

---
+++ Part 9/part09-Part09_02.PersonAndSubclasses/src/main/java/Person.java
@@ -0,0 +1,25 @@
+
+public class Person {
+    
+    private String name;
+    private String address;
+    
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + "\n" + "  " + getAddress();
+    }
+
+}
